974518a2d855d4942d48d861776cbc55e64f2899
cwohlgemuth22 <cwohlgemuth22@amherst.edu>
2020-09-18 18:12:22 +0000

    Implement string_length assembly code

--- string-length.asm
+++ string-length.asm
@@ -12,5 +12,19 @@
 
 ;;; COMPLETE THE string_length FUNCTION.
 string_length:
+        ; RDI holds a pointer to the first character of the input string. 
+        ; => So to traverse that string, need to increment the pointer
+        ; if want to compare value *in* the REGISTER (the address of a character in memory), need to dereference: byte[REGISTER]        
+        push    rdi             ; pointer to the string --> store register out onto stack
+        mov     eax, 0          ; byte count stored as the address in eax
+        dec     rdi             ; want to be before first char in string when counting
+count_char:
+        inc     eax             ; looped through a char => inc (byte count) the address in eax
+        inc     rdi             ; traverse to next char in string by incrementing pointer to (char along the) string
+        cmp     byte[rdi], 0    ; compare the byte that rdi points to (char) at this address to 0 (end of string delimiter)
+        jne     count_char      ; IF not end of string, jump to continue counting characters along the string
+        dec     eax             ; ELSE finish counting
+        pop     rdi
+        ret
 
 ;;; ----------------------------------------------------------------------------------------
